f9ae608376cbf7256d1ff0e720594b0d20f60c19
ShivShankarDash <dashshiv20@gmail.com>
2025-03-10 23:05:34 +0530

    Added changes for node projects project structure

--- github-activity/src/index.ts
+++ github-activity/src/index.ts
@@ -7,8 +7,82 @@ const rl = readline.createInterface({
     output : process.stdout
 })
 
+const EventsDescription: Record<string, string> = {
+    PushEvent: "Pushed ${count} commits to ${name}",
+    CreateEvent: "Created repository ${name}",
+    IssueEvent: "Opened ${count} issues in ${name}",
+    PullRequestEvent: "Opened ${count} pull requests in ${name}",
+    WatchEvent: "Watched ${name}",
+};
+
+interface Project{
+    repositoryId : number,
+    projectName : string,
+    count : number
+}
+
+let record : Record<string, Project[]> = {}; 
+
 const userName = process.argv[2];
 
 const gitUrl = 'https://api.github.com/users/'
-const result = fetch(gitUrl + userName + '/events');
 
+async function getGithubUserData(){
+    try {
+        const response = await fetch(gitUrl + userName + '/events');
+        const json = await response.json();
+        getRecordForEvents(json);
+    }
+    catch(error){
+        console.log(error);
+    }
+}
+
+function getRecordForEvents(json : any){
+   
+    for(let i = 0; i < json.length; i++){
+        const obj = json[i];
+        const event = obj.type;
+        upsertProjects(event, obj.repo.id, obj.repo.name);
+    }
+
+    const result : any = formatOutputMessage(record);
+    console.log(result);
+}
+
+function upsertProjects (event : string, repoId : number, projectName : string,  increment : number = 1){
+
+ if(!record[event]){
+    record[event] = [];
+ }
+
+ const projects = record[event];
+ const project = projects.find(proj => proj.repositoryId === repoId);
+
+ if(project){
+    project.count += increment;
+ }
+ else {
+    projects.push({ repositoryId : repoId, projectName, count : increment});
+ }
+
+}
+
+function formatOutputMessage(record : Record<string, Project[]>){
+
+const result : string[] = [];
+for (const [event, projects] of Object.entries(record)){
+    for(const project of projects){
+        if(EventsDescription[event]){
+        const message = EventsDescription[event]?.replace("${count}", project.count.toString()).replace("${name}", project.projectName);
+        result.push(message);
+        }
+        
+    }
+}
+return result;
+}
+
+
+
+getGithubUserData();
