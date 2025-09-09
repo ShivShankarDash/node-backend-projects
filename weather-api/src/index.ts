49de7fd1be6fdabcfe0b97eb5f7a8fbd299fca50
ShivShankarDash <dashshiv20@gmail.com>
2025-09-09 15:27:05 +0530

    Added changes for weather api and redis cache support

--- weather-api/src/index.ts
+++ weather-api/src/index.ts
@@ -1,19 +1,42 @@
 import express from 'express'
 import axios from 'axios'
 import cors from "cors";
-
+import dotenv from "dotenv"
+import { createClient } from 'redis';
 const app = express();
+dotenv.config(); 
 app.use(express.json());
 app.use(cors());
 const PORT = 3000;
+const client = createClient();
 const apiKey = process.env.WEATHER_API_KEY;
+(async () => {
+    await client.connect();
+})();
 app.get("/api/v1/weather/:location/:date1/:date2", async (req,res)=>{
-    const location : string = req.params.location
-    const date1 = req.params.date1
-    const date2 = req.params.date2 ? req.params.date2 : ""
-    const weatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}/${date1}/${date2}?key=${apiKey}`  
-    const weatherData = await axios.get(weatherUrl);
-    res.status(200).send(weatherData.data);
+    const { location, date1, date2 } = req.params;
+    let weatherUrl = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}`;
+    if (date1) {
+        weatherUrl += `/${date1}`;
+        if (date2) {
+            weatherUrl += `/${date2}`;
+        }
+    }
+    weatherUrl += `?key=${apiKey}`;
+    try{
+        const redisCache = await client.get(location);
+        if(!redisCache){
+            const weatherData = await axios.get(weatherUrl);
+            client.set(location, JSON.stringify(weatherData.data));
+            res.status(200).send(weatherData.data);
+        } 
+        else{
+            console.log(`Cache hit with the key${location}`)
+            res.status(200).send(JSON.parse(redisCache));
+        }
+    }catch(err){
+        console.log(err);
+    }
 })
 
 
